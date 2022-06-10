d9c4d3a918bfa946fcbf1f23755170f12f0491b5
Alexander von Below <alex@vonbelow.com>
2022-06-10 12:02:16 +0200

    Initial Commit

---
+++ GeofenceTester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceTester
+//
+//  Created by Alexander von Below on 10.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
