15c9e19a04cb17e5969f48814bcd3d87a3dc1d12
Alexander von Below <alex@vonbelow.com>
2022-06-26 20:00:09 +0200

    Work checkin

--- GeofenceTester/RegionsDetailControllerViewController.swift
+++ GeofenceTester/RegionsDetailControllerViewController.swift
@@ -11,16 +11,27 @@ import MapKit
 class RegionsDetailControllerViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
-    var region: CLRegion? {
+    var regions: Set<CLRegion>? {
         didSet {
-            self.updateMap()
+           // self.updateMap()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = "Monitored Regions"
+        
+        registerMapAnnotationViews()
+        
         updateMap()
+    }
+    
+    let defaultReuseIdentifier = "defaultAnnotationView"
+    private func registerMapAnnotationViews() {
+        mapView.register(MKMarkerAnnotationView.self,
+                         forAnnotationViewWithReuseIdentifier: defaultReuseIdentifier)
+//        mapView.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(SanFranciscoAnnotation.self))
     }
     
     func updateMap() {
@@ -28,10 +39,47 @@ class RegionsDetailControllerViewController: UIViewController, MKMapViewDelegate
             return
         }
         
-        if let region = region as? CLCircularRegion {
-            let overlay = MKCircle(center: region.center, radius: region.radius)
-            mapView.addOverlay(overlay)
+        guard let regions = self.regions else {
+            return
         }
+        
+        var mapAnnotations = [MKAnnotation]()
+        
+        for region in regions {
+            if let region = region as? CLCircularRegion {
+//                let overlay = MKCircle(center: region.center, radius: region.radius)
+//                mapView.addOverlay(overlay)
+                let annotation = MKPointAnnotation()
+                annotation.title = region.identifier
+                annotation.coordinate = region.center
+                mapAnnotations.append(annotation)
+            }
+        }
+        mapView.addAnnotations(mapAnnotations)
+        mapView.showAnnotations(mapAnnotations, animated: true)
+    }
+    
+    func coordinate(from coordinate: CLLocationCoordinate2D, latitudeDistance: CLLocationDistance, longitudeDistance: CLLocationDistance) -> CLLocationCoordinate2D {
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: latitudeDistance, longitudinalMeters: longitudeDistance)
+
+        let newLatitude = coordinate.latitude + region.span.latitudeDelta
+        let newLongitude = coordinate.longitude + region.span.longitudeDelta
+
+        return CLLocationCoordinate2D(latitude: newLatitude, longitude: newLongitude)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !annotation.isKind(of: MKUserLocation.self) else {
+            // Make a fast exit if the annotation is the `MKUserLocation`, as it's not an annotation view we wish to customize.
+            return nil
+        }
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: defaultReuseIdentifier,
+                                                                   for: annotation)
+        annotationView.canShowCallout = true
+        let rightButton = UIButton(type: .detailDisclosure)
+        annotationView.rightCalloutAccessoryView = rightButton
+        
+        return annotationView
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
@@ -40,6 +88,16 @@ class RegionsDetailControllerViewController: UIViewController, MKMapViewDelegate
         renderer.strokeColor = UIColor.blue
         renderer.lineWidth = 2
         return renderer
+    }
+    
+    func mapView(_ mapView: MKMapView,
+           annotationView view: MKAnnotationView,
+                          calloutAccessoryControlTapped control: UIControl){
+        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     /*
